a6943111849e500371a88fa461c4b382f4dcd87e
Alejandra Camacho <camacho.ale94@gmail.com>
2020-08-13 00:34:26 -0500

    Minior changes

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-Food App
+# Food App
+![](https://img.shields.io/github/license/alexcamachogz/food-app?style=for-the-badge)
+![](https://img.shields.io/github/stars/alexcamachogz/food-app?style=for-the-badge)
+![](https://img.shields.io/github/forks/alexcamachogz/food-app?style=for-the-badge)
+
+## 🚀 Getting Started
+
+Run the following command to install all dependencies.
+```
+$ npm install
+```
+
+## 🎨 Preview
+
+![](public/images/preview/Desktop.jpg)
--- postcss.config.js
+++ postcss.config.js
@@ -4,12 +4,12 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
-        purgecss({
-            content: ['./**/*.html'],
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-        }),
-        require('cssnano')({
-            preset: 'default',
-        }),
+        // purgecss({
+        //     content: ['./**/*.html'],
+        //     defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        // }),
+        // require('cssnano')({
+        //     preset: 'default',
+        // }),
     ],
 };
