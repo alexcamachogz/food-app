6ec30c81009ad50009ea5828642686304452d713
Alejandra Camacho <camacho.ale94@gmail.com>
2020-08-12 22:14:58 -0500

    Final settings, nanocss y purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,15 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('autoprefixer')
+        require('autoprefixer'),
+        purgecss({
+            content: ['./**/*.html'],
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+        }),
+        require('cssnano')({
+            preset: 'default',
+        }),
     ],
 };
